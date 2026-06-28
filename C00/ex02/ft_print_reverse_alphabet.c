27620f97638821ca92d4fb437b21fb8a1a7844e0
krokusB <xoio.creaturi@gmail.com>
2026-06-28 11:52:56 +0200

    reverse alphabet added

--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -1,3 +1,25 @@
 #include <unistd.h>
 
+void ft_print_reverse_alphabet(void);
 
+int main(void)
+    {
+        ft_print_reverse_alphabet();
+        return (0);
+    }
+
+void ft_print_reverse_alphabet(void)
+    {
+        char    letter;
+        letter = 122;
+        
+        int i;
+        i = 25;
+
+        while(i >= 0)
+        {
+            write(1, &letter, 1);
+            i--;
+        }    
+    }
+//brauch keinen extra i counterr
